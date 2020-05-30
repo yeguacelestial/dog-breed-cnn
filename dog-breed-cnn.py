b8e2d6fe3e88b295cb24ca74040c2d28d32e87f3
Yegua Celestial <yeguacelestial@gmail.com>
2020-05-29 20:05:15 -0500

    Added main libraries

---
+++ dog-breed-cnn.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+import time
+from datetime import timedelta
+
+import math
+import os
+
+import scipy.misc
+from scipy.stats import itemfreq
+from random import sample
+
+import pickle
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+# Image manipulation
+import PIL.Image
+from IPython.display import display
+
+from zipfile import ZipFile
+from io import BytesIO
