e974bcda4550235907479aa887dabc8d4aef500b
Yegua Celestial <yeguacelestial@gmail.com>
2020-05-30 15:03:02 -0500

    Tested dog and humans accuracy

--- dog-breed-cnn.py
+++ dog-breed-cnn.py
@@ -17,4 +17,12 @@ def load_dataset(path):
     dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
     return dog_files, dog_targets
 
+
 # STEP 1 - Detect humans
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
